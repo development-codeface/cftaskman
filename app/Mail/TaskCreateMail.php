9fe9659f1db79c2228308df677b959122080273d
Sreedevi-SM <sreeammu94@gmail.com>
2025-12-23 14:06:53 +0530

    email fix

--- app/Mail/TaskCreateMail.php
+++ app/Mail/TaskCreateMail.php
@@ -41,7 +41,7 @@ class TaskCreateMail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'emails.task_created',
+            view: 'emails.task_create',
         );
     }
 
